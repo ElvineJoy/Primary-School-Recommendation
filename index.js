e3b8dd5f350c1f1ffd8294768be423eb0803d1f2
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-27 22:29:34 +0300

    Update of html and css files

---
+++ index.js
@@ -0,0 +1,2 @@
+const schoolApi= fetch("https://energydata.info/dataset/2fda191d-c3c6-4002-8c82-daa02008a9e3/resource/129b8c79-de8b-4b7b-8310-cdd207e46863/download/schools.json");
+console.log(schoolApi);
