436d14e1be890f7621ddb9a4094302f3565a15b2
ElvineJoy <elvinkuiyungest@gmail.com>
2024-04-30 16:26:33 +0300

    Adding an Event listener

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
  document.addEventListener('DOMContentLoaded',() => {
-    alert("DOM ready!");
+    document.querySelector('form').addEventListener('submit', (e) =>{
+      console.log(e)
+    })
  });
 
 
 
 
-const schoolApi= fetch("https://energydata.info/dataset/2fda191d-c3c6-4002-8c82-daa02008a9e3/resource/129b8c79-de8b-4b7b-8310-cdd207e46863/download/schools.json");
-console.log(schoolApi);
+// const schoolApi= fetch("https://energydata.info/dataset/2fda191d-c3c6-4002-8c82-daa02008a9e3/resource/129b8c79-de8b-4b7b-8310-cdd207e46863/download/schools.json");
+// console.log(schoolApi);
