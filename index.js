4a6337339eed2320ef7fd312da9cba9785444744
ElvineJoy <elvinkuiyungest@gmail.com>
2024-05-02 17:10:17 +0300

    Filling the search options achieved

--- index.js
+++ index.js
@@ -1,29 +1,79 @@
-//  document.addEventListener('DOMContentLoaded',() => {
-//     document.querySelector('form').addEventListener('submit', (e) =>{
-//       console.log(e)
-//     })
-//  });
+ document.addEventListener('DOMContentLoaded', () => {
+  
+  fetch("https://energydata.info/dataset/2fda191d-c3c6-4002-8c82-daa02008a9e3/resource/129b8c79-de8b-4b7b-8310-cdd207e46863/download/schools.json")
+  .then(response => response.json())
+  .then(data => {
+
+    const featuresArray = data.features;
+
+    const propertiesArray = featuresArray.map(feature => feature.properties);
+    
+    function getUniqueValues(array, key) {
+      return Array.from(new Set(array.map(item => item[key])));
+  }
+  
+   function getSelectionOptions(selectId, values) {
+    const select = document.getElementById(selectId);
+    values.forEach(function(value) {
+        const option = document.createElement('option');
+        option.text = value;
+        select.add(option);
+    });
+}
+      getSelectionOptions('county', getUniqueValues(propertiesArray, "County"));
+      getSelectionOptions('Sub', getUniqueValues(propertiesArray, "SUB_COUNTY"));
+      getSelectionOptions('Level', getUniqueValues(propertiesArray,"LEVEL"));
+      getSelectionOptions('Status', getUniqueValues(propertiesArray,"Status"));
+     
+  })
+  .catch(error => {
+    console.error('Error Fetching Data', error)
+  });
+});
 
 
+ 
+  
+ const searchForm = document.querySelector("#form");
 
-// function init
-const initialize = () => {
-  const serachForm = document.querySelector("#form")
-
-  serachForm.addEventListener('sumbit', (e) => {
+  searchForm.addEventListener("sumbit", (e) => {
     e.preventDefault();
+
+    // getting the values or each select element
+    const selectedCounty = document.getElementById("county").value;
+    const selectedSubCounty = document.getElementById("Sub").value;
+    const selectedLevel = document.getElementById("Level").value;
+    const selectedStatus = document.getElementById("Status").value;
+
+    retrieveData(selectedCounty, selectedSubCounty, selectedLevel, selectedStatus);
+  });
+
+  function retrieveData (selectedCounty, selectedSubCounty, selectedLevel, selectedStatus) {
 
     fetch("https://energydata.info/dataset/2fda191d-c3c6-4002-8c82-daa02008a9e3/resource/129b8c79-de8b-4b7b-8310-cdd207e46863/download/schools.json")
     .then((response) => response.json())
-    .then((schoolsData) => console.log(schoolsData))
+    .then((schoolsData) => {
+      document.getElementById("result").innerHTML = JSON.stringify(schoolsData);
+    })
+    .catch((error) => {
+      alert("unsuccessful!")
+      console.log(error.message)
+    } 
+    
+  )
+  }
 
-  })
-}
+    
+
+
+// //  const displayOptions = (schoolArray) => {
+// //   schoolArray.forEach(element => {
+// //     const optionTag = document.createElement("option")
+
+// //   });
+// //  }
 
 
 
 
-
-
-
-document.addEventListener("DOMContentLoaded", initialize);
+// // document.addEventListener("DOMContentLoaded", initialize);
